d7f968fa11fc590754dc2de5f5be31d6273eac9d
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-09-04 11:11:12 +0700

    feat: add BookCard component

---
+++ components/BookCard.jsx
@@ -0,0 +1,22 @@
+// TODO: fixed w and h?
+export default function BookCard({ title, author, imageUrl, reverse = true }) {
+  return (
+    <div className={`flex flex-col h-60 w-24 gap-1`}>
+      <div
+        className={`w-24 ${
+          reverse ? "rounded-b-full order-1" : "rounded-t-full order-2"
+        }  overflow-x-hidden overflow-y-visible`}
+      >
+        <img src={imageUrl} alt={`${title} by ${author}`} className="w-full " />
+      </div>
+      <div
+        className={`${
+          reverse ? "order-2" : "order-1"
+        } text-[0.5rem] flex flex-col items-center`}
+      >
+        <p className="font-semibold">{title}</p>
+        <p>{author}</p>
+      </div>
+    </div>
+  );
+}
