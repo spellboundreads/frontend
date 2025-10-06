7e0366cfa5e8e955642c946c37c0ad1345a6049d
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-06 15:49:17 +0700

    chore(works): rename WorkGenres to WorkSubjects

---
+++ components/work/WorkSubjects.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Chip from "@/components/Chip";
+import { useState } from "react";
+
+type WorkSubjects = {
+  subjects: string[];
+};
+
+export default function WorkSubjects({ subjects }: WorkSubjects) {
+  const defaultLength = 15;
+  const [fullDisplay, setFullDisplay] = useState(false);
+
+  return (
+    <div className="hidden md:flex flex-col gap-2 px-2">
+      <h2 className="font-semibold">Subjects</h2>
+      <div className="flex flex-wrap gap-1">
+        {(fullDisplay ? subjects : subjects.slice(0, defaultLength)).map(
+          (subject) => (
+            <Chip key={subject} href="#">
+              {subject}
+            </Chip>
+          )
+        )}
+        {subjects.length > defaultLength && !fullDisplay && (
+          <button
+            className=" ml-4 uppercase font-semibold text-gray-800 shadow-2xl"
+            onClick={() => setFullDisplay(true)}
+          >
+            See more
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
