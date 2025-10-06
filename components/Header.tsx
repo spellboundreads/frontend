37c642574ce22bbba55997b7693e278ab9a03480
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-06 15:05:32 +0700

    feat(Header): refactor Header component to accept login and register click handlers

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,35 @@
 import { useAuth } from "@/context/AuthContext";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function Header() {
-  const { user, logout } = useAuth();
-  console.log("Header user:", user);  
+export default function Header({
+  onLoginClick,
+  onRegisterClick,
+}: {
+  onLoginClick?: () => void;
+  onRegisterClick?: () => void;
+}) {
+  const { user, login, register } = useAuth();
 
   return (
-    <div className="text-gray-700 shadow-sm bg-white flex justify-between items-center py-4 px-8 border-b border-gray-300 sticky top-0 z-50">
-      <SearchBar />
+    <div className="px-2 text-gray-700 shadow-sm h-20 bg-white flex justify-end gap-4 items-center  border-b border-gray-300 sticky top-0 z-50">
       {user ? <UserInformation user={user} /> : <div>Hi</div>}
+      {!user && (
+        <>
+          <button
+            className="rounded-md uppercase font-semibold"
+            onClick={onLoginClick}
+          >
+            Sign in
+          </button>
+          <button
+            className="rounded-md uppercase font-semibold"
+            onClick={onRegisterClick}
+          >
+            Create Account
+          </button>
+        </>
+      )}
+      <SearchBar />
     </div>
   );
 }
@@ -19,14 +40,14 @@ function UserInformation({
 }: {
   user: { username: string; avatar_url?: string } | null;
 }) {
-  if (!user) return null; // or render a placeholder
+  if (!user) return null;
 
   return (
     <div className="flex items-center gap-3">
       <img
-        src={user.avatar_url || "/globe.svg"} // fallback in public folder
+        src={user.avatar_url || "/globe.svg"}
         alt="User Avatar"
-        className="w-10 h-10 rounded-full object-cover"
+        className="w-8 h-8 rounded-full object-cover"
       />
       <div className="max-w-42 truncate block font-semibold">
         {user.username}
@@ -37,11 +58,11 @@ function UserInformation({
 
 function SearchBar() {
   return (
-    <div className="flex items-center border border-gray-300 rounded-full py-2 px-4 w-96">
+    <div className="flex items-center border border-gray-300 rounded-full py-1 px-2 w-56">
       <SearchIcon className="text-gray-500" />
       <input
         type="text"
-        placeholder="Search books, authors, genres..."
+        placeholder="Search..."
         className="flex-1 outline-none border-none ml-2 text-gray-700 "
       />
     </div>
