d994bb17a8184212df42b1ed13459b60501da017
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-08 20:13:02 +0700

    feat(chip): add link functionality to Chip component

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -1,7 +1,14 @@
-export default function Chip({ children, href }) {
+import Link from "next/link";
+export default function Chip({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href?: string;
+}) {
   return (
     <div className="bg-white text-gray-800 px-4 py-1 border-gray-500 rounded-full text-sm border-1 hover:shadow-md cursor-pointer">
-      {children}
+      {href ? <Link href={href}>{children}</Link> : children}
     </div>
   );
 }
