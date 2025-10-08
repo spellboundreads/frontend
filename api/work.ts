660a1e29697965533381388dff8442ae761ed80f
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-08 18:49:18 +0700

    chore(api): remove types from api to use imports instead

--- api/work.ts
+++ api/work.ts
@@ -1,58 +1,15 @@
 import apiClient from "@/lib/apiClient";
+import { Work } from "@/types/work";
+import {GetWorkResponse, SearchWorkResponse} from "@/types/api";
 
-export interface Work {
-  data: {
-    id: string;
-    openlibrary_id: string;
-    title: string;
-    first_publish_date: string;
-    covers: string[];
-    description: string;
-    excerpts: string[];
-    created_at: string;
-    updated_at: string;
-    subjects: string[];
-    works_authors: {
-      authors: {
-        name: string;
-        photos: string[];
-        bio: string;
-      };
-    };
-  };
-}
-
-export interface SearchWorkResponse {
-  data?: {
-    numFound: number;
-    start: number;
-    docs: SearchWorkEntry[];
-  };
-}
-
-export interface SearchWorkEntry {
-  title: string;
-  cover_i: string;
-  author_name: string[];
-  author_key: string[];
-  first_publish_year?: number;
-  key: string;
-}
-
-interface SearchWorkQuery {
-  title?: string;
-  language?: string;
-  limit?: number;
-  page?: number;
-}
 
 export const findWorks = async (query: string): Promise<SearchWorkResponse> => {
   const response = await apiClient.get<SearchWorkResponse>(`/works?${query}`);
   return response.data;
 };
 
-export const getWork = async (id: string): Promise<Work> => {
-  const response = await apiClient.get<Work>(`/works/${id}`);
+export const getWork = async (id: string): Promise<GetWorkResponse> => {
+  const response = await apiClient.get<GetWorkResponse>(`/works/${id}`);
   return response.data;
 };
 
