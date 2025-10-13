492e541e91e4e48e1ae2c7e0cb24232c150e9a27
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-13 23:26:41 +0700

    feat(review): create ReviewCard

---
+++ components/work/ReviewCard.tsx
@@ -0,0 +1,57 @@
+import Rating from "@mui/material/Rating";
+import Link from "next/link";
+
+interface ReviewCardProps {
+  userId: string;
+  userAvatar: string;
+  userDisplayName: string;
+  createdAt: string;
+  reviewText: string;
+  rating: number;
+}
+
+export default function ReviewCard({
+  userId,
+  userAvatar,
+  userDisplayName,
+  createdAt,
+  reviewText,
+  rating,
+}: ReviewCardProps) {
+  return (
+    <div className="flex ">
+      {/* User Avatar */}
+      <div>
+        <div className="w-12 h-12 rounded-full bg-gray-300 overflow-hidden">
+          <img
+            src={userAvatar}
+            alt={userDisplayName}
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col gap-2 ml-4">
+          <div className="flex gap-2 items-center">
+            <p className="text-sm text-gray-500">
+              Review by{" "}
+              <Link
+                href={`/users/${userId}`}
+                className="font-semibold hover:underline"
+              >
+                {userDisplayName}
+              </Link>
+            </p>
+            <Rating
+              name="read-only"
+              value={rating / 2}
+              readOnly
+              precision={0.5}
+            />
+          </div>
+          <div className="text-sm text-gray-800">{reviewText}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
