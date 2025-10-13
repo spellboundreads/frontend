e84998ba7f102435e36bd8ff02824cb2baf81c7d
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-14 01:55:15 +0700

    fix(search): implement simple normalization for data

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,7 @@
 import WorkCard from "@/components/search/WorkCard";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState, Suspense } from "react";
-import { findWorks, getImage } from "@/api/work";
+import { findWorks, getImage, getWork } from "@/api/work";
 import { SearchWorkResponse } from "@/types/api";
 
 function SearchResults() {
@@ -16,20 +16,36 @@ function SearchResults() {
     async function fetchWorks() {
       try {
         const response: SearchWorkResponse = await findWorks(query);
-        const normalizedWorks = response.data?.docs.map((doc) => ({
-          work_key: doc.key,
-          title: doc.title,
-          first_published_year: doc.first_publish_year,
-          cover: doc.cover_i ? getImage(doc.cover_i.toString()) : null,
-          authors: doc.author_name
-            ? doc.author_name.map((a_name: string, index: number) => ({
-                author_name: a_name,
-                author_key: doc.author_key[index],
-              }))
-            : [],
-        }));
+        const normalizedWorks: {
+          work_key: string;
+          title: string;
+          first_published_year?: string;
+          authors: { author_name: string; author_key: string }[];
+          cover?: string | null;
+          description?: string;
+        }[] = [];
 
-        setWorks(normalizedWorks || []);
+        if (response.data?.docs) {
+          for (const doc of response.data.docs) {
+            const workDetails = await getWork(doc.key.replace("/works/", ""));
+            normalizedWorks.push({
+              work_key: doc.key,
+              title: workDetails.data.title,
+              first_published_year:
+                workDetails.data.first_published_date || undefined,
+              authors: workDetails.data.works_authors.map((wa) => ({
+                author_name: wa.authors.name,
+                author_key: wa.authors.openlibrary_id,
+              })),
+              cover: workDetails.data.covers?.[0]
+                ? getImage(workDetails.data.covers[0])
+                : null,
+              description: workDetails.data.description || undefined,
+            });
+          }
+        }
+
+        setWorks(normalizedWorks);
       } catch (err) {
         console.error(err);
       }
@@ -39,20 +55,23 @@ function SearchResults() {
 
   return (
     <div className="flex gap-4 px-32 py-6 text-black">
-      <div className="w-xl flex flex-col gap-8">
+      <div className="w-2xl flex flex-col gap-8">
         <div className="border-b p-2">
           <p>Showing results for search</p>
         </div>
-        {works.map((work, index) => (
-          <WorkCard
-            key={index}
-            work_key={work.work_key}
-            title={work.title}
-            first_published_year={work.first_published_year}
-            authors={work.authors}
-            cover={work.cover}
-          />
-        ))}
+        <div className="flex flex-col gap-4">
+          {works.map((work, index) => (
+            <WorkCard
+              key={index}
+              work_key={work.work_key}
+              title={work.title}
+              first_published_year={work.first_published_year}
+              authors={work.authors}
+              cover={work.cover}
+              description={work.description}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
