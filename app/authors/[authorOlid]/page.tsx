3215adb28e50f92406283ae9e41d85a8fb8dd3f4
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-19 23:58:13 +0700

    feat(author): add AuthorDescriptionDialog for displaying full author bio

--- app/authors/[authorOlid]/page.tsx
+++ app/authors/[authorOlid]/page.tsx
@@ -6,7 +6,17 @@ import { getAuthor, getAuthorWorks } from "@/api/author";
 import { GetAuthorWorkResponse } from "@/types/api";
 import { Author } from "@/types/author";
 import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import WorkCard from "@/components/search/WorkCard";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function AuthorWorksSection() {
   const { authorOlid } = useParams<{ authorOlid: string }>();
@@ -46,50 +56,54 @@ export default function AuthorWorksSection() {
     fetchAuthorWorks();
   }, []);
 
-  return (
-    author && (
-      <div className="flex flex-col">
-        <div className="w-full bg-[#eae7da]">
-          <AuthorOverview
-            name={author.name}
-            bio={author.bio || undefined}
-            portrait={
-              author.photos && author.photos.length > 0
-                ? getImage(author.photos[0]) || undefined
-                : undefined
-            }
-          />
-        </div>
-        {authorWorks && (
-          <div className="p-4 flex">
-            {/* Authors' works */}
-            <div className="mx-16">
-              <h2 className="text-2xl font-semibold w-full mb-4">
-                Showing {author.name}'s works
-              </h2>
-              <div className="flex flex-col gap-4 w-full">
-                {authorWorks.entries.slice(0, 5).map((work) => (
-                  <WorkCard
-                    key={work.key.split("/").pop()}
-                    work_key={work.key.split("/").pop() || ""}
-                    title={work.title}
-                    first_publish_year={work.first_publish_year}
-                    cover={
-                      work.covers && work.covers.length > 0
-                        ? getImage(work.covers[0].toString())
-                        : undefined
-                    }
-                    description={
-                      work.description ? work.description : undefined
-                    }
-                  />
-                ))}
-              </div>
+  return author ? (
+    <div className="flex flex-col">
+      <div className="w-full bg-[#eae7da]">
+        <AuthorOverview
+          name={author.name}
+          bio={author.bio || undefined}
+          portrait={
+            author.photos && author.photos.length > 0
+              ? getImage(author.photos[0]) || undefined
+              : undefined
+          }
+        />
+      </div>
+      {authorWorks ? (
+        <div className="px-24 flex py-8">
+          {/* Authors' works */}
+          <div className="mx-16">
+            <h2 className="text-2xl font-semibold w-full mb-4">
+              Showing {author.name}'s works
+            </h2>
+            <div className="flex flex-col gap-4 max-w-4xl">
+              {authorWorks.entries.slice(0, 5).map((work) => (
+                <WorkCard
+                  key={work.key.split("/").pop()}
+                  work_key={work.key.split("/").pop() || ""}
+                  title={work.title}
+                  first_publish_year={work.first_publish_year}
+                  cover={
+                    work.covers && work.covers.length > 0
+                      ? getImage(work.covers[0].toString())
+                      : undefined
+                  }
+                  description={work.description ? work.description : undefined}
+                />
+              ))}
             </div>
           </div>
-        )}
-      </div>
-    )
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-96">
+          <Spinner className="size-8" />
+        </div>
+      )}
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-96">
+      <Spinner className="size-8" />
+    </div>
   );
 }
 
@@ -114,9 +128,32 @@ function AuthorOverview({ name, portrait, bio }: AuthorOverviewProps) {
         {bio && (
           <>
             <p className="text-sm line-clamp-10">{bio}</p>
+            <AuthorDescriptionDialog name={name} bio={bio} />
           </>
         )}
       </div>
     </div>
+  );
+}
+
+function AuthorDescriptionDialog({ name, bio }: { name: string; bio: string }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button className="bg-transparent text-xs text-gray-700 underline black hover:underline w-fit text-left">
+          See full details
+        </button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-xl text-center">
+            About {name}
+          </DialogTitle>
+          <DialogDescription className="mt-2 text-xs text-gray-800">
+            {bio}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
