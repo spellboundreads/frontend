138c24ea13ad0735c4e05f0cadc90b6fcd5d2a4c
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-19 20:08:06 +0700

    fix(workcard): correct author prop type and improve image handling

--- components/search/WorkCard.tsx
+++ components/search/WorkCard.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-import { Author } from "@/types/work";
-import { getImage } from "@/api/work";
-
 interface WorkCardProps {
   title: string;
-  first_published_year?: number;
+  first_publish_year?: number;
   description?: string;
-  authors: Author[];
+  authors?: {
+    key: string;
+    name: string;
+  }[];
   cover?: string;
   work_key: string;
 }
@@ -20,45 +20,47 @@ interface AuthorChipProps {
 export default function WorkCard({
   work_key,
   title,
-  first_published_year,
+  first_publish_year,
   description,
   authors,
   cover,
 }: WorkCardProps) {
-  console.log("authors in WorkCard:", authors);
+  console.log("key in WorkCard:", work_key);
   return (
     <div className="flex gap-6 w-full h-56 text-black text-xs border-b-1 border-gray-400 p-4">
       <div className="min-w-32 max-w-32 bg-white flex justify-center ">
         <img
-          src={cover}
+          src={cover ? cover : "/placeholder/work.png"}
           alt={`Cover of ${title}`}
           className="h-full object-cover"
         />
       </div>
 
       <div className="flex flex-col gap-2">
-        <Link href={`/works/${work_key}`}>
+        <Link href={`/works/${work_key}`} replace>
           <div className="flex gap-2">
             <h2 className="text-sm font-semibold">{title}</h2>
-            <p className="text-sm ">{first_published_year}</p>
+            <p className="text-sm ">{first_publish_year}</p>
           </div>
         </Link>
         <p className="line-clamp-5 break-words overflow-hidden">
           {description}
         </p>
-        <div className="flex gap-2 max-h-6 items-center">
-          <span className="text-xs">Written by</span>
-          {authors.slice(0, 2).map((author, index) => (
-            <AuthorChip
-              key={index}
-              author_name={author.name}
-              author_key={author.openlibrary_id}
-            />
-          ))}
-          {authors.length > 2 && (
-            <span className="text-xs">and {authors.length - 2} more</span>
-          )}
-        </div>
+        {authors && (
+          <div className="flex gap-2 max-h-6 items-center">
+            <span className="text-xs">Written by</span>
+            {authors.slice(0, 2).map((author, index) => (
+              <AuthorChip
+                key={index}
+                author_name={author.name}
+                author_key={author.key}
+              />
+            ))}
+            {authors.length > 2 && (
+              <span className="text-xs">and {authors.length - 2} more</span>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
