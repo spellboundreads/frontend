7e89cda9c40a898333fcb8c0ea4bd5f173874bc9
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-19 21:47:52 +0700

    fix(review): add getReviewByUserWork function and update imports

--- api/review.ts
+++ api/review.ts
@@ -1,7 +1,21 @@
 import apiClient from "@/lib/apiClient";
-import { CreateReviewPayload } from "@/types/review";
+import {
+  CreateReviewPayload,
+  GetReviewByUserWorkResponse,
+  Review,
+} from "@/types/review";
 
 export const createReview = async (payload: CreateReviewPayload) => {
   const response = await apiClient.post("/reviews", payload);
+  return response;
+};
+
+export const getReviewByUserWork = async (
+  workId: string,
+  userId: string
+): Promise<GetReviewByUserWorkResponse> => {
+  const response = await apiClient.get<GetReviewByUserWorkResponse["data"]>(
+    `/reviews/by-user-work/${userId}/${workId}`
+  );
   return response;
 };
