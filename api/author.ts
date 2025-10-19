e53b190fcfa23a48d5f783f9e5ce89502de4dee7
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-19 20:07:31 +0700

    feat(author): add getAuthorWorks API function to fetch works by author

--- api/author.ts
+++ api/author.ts
@@ -1,4 +1,5 @@
-import { GetAuthorResponse } from "@/types/api";
+import { GetAuthorResponse, GetAuthorWorkResponse } from "@/types/api";
+
 import apiClient from "@/lib/apiClient";
 
 export const getAuthor = async (
@@ -6,6 +7,15 @@ export const getAuthor = async (
 ): Promise<GetAuthorResponse> => {
   const response = await apiClient.get<GetAuthorResponse["data"]>(
     `/authors/${authorOlid}`
+  );
+  return response;
+};
+
+export const getAuthorWorks = async (
+  authorOlid: string
+): Promise<GetAuthorWorkResponse> => {
+  const response = await apiClient.get<GetAuthorWorkResponse["data"]>(
+    `/authors/${authorOlid}/works`
   );
   return response;
 };
