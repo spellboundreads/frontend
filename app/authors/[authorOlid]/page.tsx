3d4ffc74df518f16ebefbb9ca20ad0e8cf5c9db2
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-19 20:07:49 +0700

    feat(author): fetch and display author's works in AuthorWorksSection

--- app/authors/[authorOlid]/page.tsx
+++ app/authors/[authorOlid]/page.tsx
@@ -2,30 +2,50 @@
 import { getImage } from "@/api/work";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { getAuthor } from "@/api/author";
+import { getAuthor, getAuthorWorks } from "@/api/author";
+import { GetAuthorWorkResponse } from "@/types/api";
 import { Author } from "@/types/author";
 import { toast } from "sonner";
+import WorkCard from "@/components/search/WorkCard";
 
 export default function AuthorWorksSection() {
-  const params = useParams<{ authorOlid: string }>();
+  const { authorOlid } = useParams<{ authorOlid: string }>();
   const [author, setAuthor] = useState<Author>();
+  const [authorWorks, setAuthorWorks] =
+    useState<GetAuthorWorkResponse["data"]>();
 
   useEffect(() => {
     async function fetchAuthor() {
       try {
-        const response = await getAuthor(params.authorOlid);
-        setAuthor(response.data);
+        const response = await getAuthor(authorOlid);
+        setAuthor((prev) => {
+          return {
+            ...prev,
+            ...response.data,
+          };
+        });
       } catch (err) {
         toast("Can't fetch author's data.");
       }
     }
 
+    async function fetchAuthorWorks() {
+      try {
+        const response = await getAuthorWorks(authorOlid);
+        setAuthorWorks(response.data);
+        console.log(response.data.entries);
+      } catch (err) {
+        toast("Can't fetch author's works.");
+      }
+    }
+
     fetchAuthor();
+    fetchAuthorWorks();
   }, []);
 
   return (
     author && (
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col">
         <div className="w-full bg-[#eae7da]">
           <AuthorOverview
             name={author.name}
@@ -37,6 +57,34 @@ export default function AuthorWorksSection() {
             }
           />
         </div>
+        {authorWorks && (
+          <div className="p-4 flex">
+            {/* Authors' works */}
+            <div className="mx-16">
+              <h2 className="text-2xl font-semibold w-full mb-4">
+                Showing {author.name}'s works
+              </h2>
+              <div className="flex flex-col gap-4 w-full">
+                {authorWorks.entries.slice(0, 5).map((work) => (
+                  <WorkCard
+                    key={work.key.split("/").pop()}
+                    work_key={work.key.split("/").pop() || ""}
+                    title={work.title}
+                    first_publish_year={work.first_publish_year}
+                    cover={
+                      work.covers && work.covers.length > 0
+                        ? getImage(work.covers[0].toString())
+                        : undefined
+                    }
+                    description={
+                      work.description ? work.description : undefined
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     )
   );
