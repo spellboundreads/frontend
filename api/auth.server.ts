cac2b61de1ff754d16cda96f2a3062246f05c5a1
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-20 04:16:57 +0700

    feat(header): implement getMeServer function to fetch user data and update Header component

---
+++ api/auth.server.ts
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import apiClient from "@/lib/apiClient";
+
+export async function getMeServer() {
+  const cookieStore = await cookies();
+  const tokenCookie = cookieStore.get("token");
+  const cookieHeader = tokenCookie ? `token=${tokenCookie.value}` : "";
+
+  const res = await apiClient.get("/users/me", {
+    headers: { Cookie: cookieHeader },
+  });
+  return res.data;
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,5 @@
-"use client";
-import { useState } from "react";
-import { useAuth } from "@/context/AuthContext";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
-import SearchIcon from "@mui/icons-material/Search";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,9 +15,16 @@ import {
   DialogHeader,
   DialogDescription,
 } from "@/components/ui/dialog";
-
-export default function Header() {
-  const { user } = useAuth();
+import UserDropdown from "@/components/user/dropdown";
+import { getMeServer } from "@/api/auth.server";
+import SearchBar from "./search/searchbar";
+export default async function Header() {
+  let user = null;
+  try {
+    user = await getMeServer();
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
 
   return (
     <header className="px-2 text-gray-700 shadow-sm py-4 bg-white flex justify-end gap-4 items-center border-b border-gray-300 sticky top-0 z-50">
@@ -48,42 +51,6 @@ export default function Header() {
       </button>
       <SearchBar />
     </header>
-  );
-}
-
-function UserDropdown({
-  user,
-}: {
-  user: { username: string; avatar_url?: string } | null;
-}) {
-  if (!user) return null;
-  function handleLogout() {
-    localStorage.removeItem("accessToken");
-    window.location.reload();
-  }
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="rounded-sm bg-transparent border-none shadow-none text-gray-800 hover:bg-[#f0f0f0] hover:text-gray-800 p-1.5">
-          <div className="flex items-center gap-3">
-            <img
-              src={user.avatar_url || "/placeholder/user.png"}
-              alt="User Avatar"
-              className="size-7 rounded-full object-cover border-1"
-            />
-            <div className="max-w-42 truncate block font-semibold">
-              {user.username}
-            </div>
-          </div>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="start">
-        <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer" onClick={handleLogout}>
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
   );
 }
 
@@ -122,18 +89,5 @@ function LoginDialog() {
         <LoginForm />
       </DialogContent>
     </Dialog>
-  );
-}
-
-function SearchBar() {
-  return (
-    <div className="flex items-center border border-gray-300 rounded-full py-1 px-2 w-42">
-      <SearchIcon className="text-gray-500" />
-      <input
-        type="text"
-        placeholder="Search..."
-        className="flex-1 outline-none border-none ml-2 text-gray-700 text-xs"
-      />
-    </div>
   );
 }
