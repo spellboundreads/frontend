1b9d57c8585e09127178ace09ad0692f5d763f7d
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-20 23:53:56 +0700

    feat(auth): add register and login functions for user authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,39 @@
 import { cookies } from "next/headers";
 import apiClient from "./apiClient";
+import { AuthResponse } from "@/types/api";
+
+interface RegisterDto {
+  email: string;
+  username: string;
+  password: string;
+  display_name: string;
+}
+
+interface LoginDto {
+  email: string;
+  password: string;
+}
+
+export const register = async (data: RegisterDto): Promise<AuthResponse> => {
+  const response = await apiClient.post<AuthResponse["data"]>(
+    "/auth/register",
+    data
+  );
+  return response;
+};
+
+export const login = async (data: LoginDto): Promise<AuthResponse> => {
+  const response = await apiClient.post<AuthResponse["data"]>(
+    "/auth/login",
+    data
+  );
+  return response;
+};
+
+export const logout = async () => {
+  const res = await apiClient.post("/auth/logout");
+  return res.data;
+};
 
 export async function getMe() {
   const cookieStore = await cookies();
