c20efd4594a43aa0e387c5d23b258dfcbe9eec5b
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-20 23:54:51 +0700

    fix(layout): wrap Header component in a div for proper positioning

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${beVietnamPro.variable} antialiased`}>
         <div className=" overflow-x-hidden relative ">
-          <Header />
+          <div className="fixed top-0 left-0 w-full z-50">
+            <Header />
+          </div>
           <Toaster />
 
           <div>{children}</div>
