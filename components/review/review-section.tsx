6cb900a1bb9267068701bf88fa44ef163ea7c785
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-21 10:18:09 +0700

    feat(reviews): add ReviewForm to ReviewSection

--- components/review/review-section.tsx
+++ components/review/review-section.tsx
@@ -1,27 +1,48 @@
+"use client";
+
 import { Review } from "@/types/review";
 import ReviewCard from "../work/ReviewCard";
+import ReviewForm from "./review-form";
+import { useState } from "react";
+import { User } from "@/types/user";
 
 interface ReviewSectionProps {
   reviews: Review[];
+  workId: string;
+  user: User | null;
 }
 
-export default function ReviewSection({ reviews }: ReviewSectionProps) {
+export default function ReviewSection({
+  reviews,
+  workId,
+  user,
+}: ReviewSectionProps) {
+  const [hasUserReviewed, setHasUserReview] = useState(
+    reviews.some((review) => review.user_id === user?.id)
+  );
+
+  console.log(hasUserReviewed);
+
   return (
-    <div>
-      <h2 className="text-2xl font-semibold">Community Reviews</h2>
-      <div className="flex flex-col gap-8 mt-4">
-        {reviews.map((review) => (
-          <ReviewCard
-            key={review.id}
-            userId={review.users.id}
-            userAvatar={review.users.avatar_url || "/placeholder/user.png"}
-            userDisplayName={review.users.display_name || review.users.username}
-            createdAt={review.created_at}
-            reviewText={review.review_text || ""}
-            rating={review.rating}
-          />
-        ))}
+    <>
+      {!hasUserReviewed && (
+        <ReviewForm workId={workId} onSubmit={() => setHasUserReview(true)} />
+      )}
+      <div>
+        <h2 className="text-2xl font-semibold">Community Reviews</h2>
+        <div className="flex flex-col gap-8 mt-4">
+          {/* {hasUserReviewed && <ReviewCard />} */}
+          {reviews.length > 0 ? (
+            reviews.map((review) => (
+              <ReviewCard key={review.id} review={review} />
+            ))
+          ) : (
+            <div className="  text-gray-800">
+              <i>No reviews have been left for this work yet.</i>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
