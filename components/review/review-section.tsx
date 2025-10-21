ef2bceb79199444b644434410190027008830e31
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-21 21:55:27 +0700

    fix: remove handle submit function to child component

--- components/review/review-section.tsx
+++ components/review/review-section.tsx
@@ -3,8 +3,10 @@
 import { Review } from "@/types/review";
 import ReviewCard from "../work/ReviewCard";
 import CreateReviewForm from "./create-review-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User } from "@/types/user";
+import { getReviewByUserWork } from "@/api/review";
+import { AxiosError } from "axios";
 
 interface ReviewSectionProps {
   reviews: Review[];
@@ -17,24 +19,36 @@ export default function ReviewSection({
   workId,
   user,
 }: ReviewSectionProps) {
-  const [hasUserReviewed, setHasUserReview] = useState(
-    reviews.some((review) => review.user_id === user?.id)
-  );
+  const [currentUserReview, setCurrentUserReview] = useState<Review | null>();
 
-  console.log(hasUserReviewed);
+  useEffect(() => {
+    async function fetchCurrentUserReview() {
+      try {
+        if (user) {
+          const response = await getReviewByUserWork(workId, user.id);
+          setCurrentUserReview(response.data);
+        }
+      } catch (error: unknown) {
+        if (error instanceof AxiosError && error.status === 404) {
+          setCurrentUserReview(null);
+        }
+      }
+    }
+    fetchCurrentUserReview();
+  }, []);
 
   return (
     <>
-      {!hasUserReviewed && (
+      {user && !currentUserReview && (
         <CreateReviewForm
           workId={workId}
-          onSubmit={() => setHasUserReview(true)}
+          onSubmit={(review) => setCurrentUserReview(review)}
         />
       )}
       <div>
         <h2 className="text-2xl font-semibold">Community Reviews</h2>
         <div className="flex flex-col gap-8 mt-4">
-          {/* {hasUserReviewed && <ReviewCard />} */}
+          {currentUserReview && <ReviewCard review={currentUserReview} />}
           {reviews.length > 0 ? (
             reviews.map((review) => (
               <ReviewCard key={review.id} review={review} />
