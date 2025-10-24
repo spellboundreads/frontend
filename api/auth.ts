4de33bf8b6f45e5f339223efec294ce17df71c38
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-24 17:52:56 +0700

    fix(api): remove redundant methods

--- api/auth.ts
+++ api/auth.ts
@@ -1,33 +1,6 @@
-import {serverApiClient} from "@/lib/apiClient.server";
+import { serverApiClient } from "@/lib/apiClient.server";
 import { AuthResponse } from "@/types/api";
-
-interface RegisterDto {
-  email: string;
-  username: string;
-  password: string;
-  display_name: string;
-}
-
-interface LoginDto {
-  email: string;
-  password: string;
-}
-
-export const register = async (data: RegisterDto): Promise<AuthResponse> => {
-  const response = await serverApiClient.post<AuthResponse["data"]>(
-    "/auth/register",
-    data
-  );
-  return response;
-};
-
-export const login = async (data: LoginDto): Promise<AuthResponse> => {
-  const response = await serverApiClient.post<AuthResponse["data"]>(
-    "/auth/login",
-    data
-  );
-  return response;
-};
+import * as z from "zod";
 
 export const logout = async () => {
   const res = await serverApiClient.post("/auth/logout");
