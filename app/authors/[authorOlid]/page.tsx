f33243da1c54b3017273a80f5e0ee611bb9ba1f1
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-24 17:54:46 +0700

    feat(author): add author works section

--- app/authors/[authorOlid]/page.tsx
+++ app/authors/[authorOlid]/page.tsx
@@ -10,6 +10,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import AuthorsWorksSection from "@/components/author/authors-works-section";
 
 import WorkCard from "@/components/search/WorkCard";
 import { Spinner } from "@/components/ui/spinner";
@@ -31,25 +32,6 @@ export default async function Page({
     );
   }
 
-  const entries = authorWorks.entries;
-  const isAuthorWorkEntries = entries.length > 0 && "key" in entries[0]; // runtime type guard
-
-  const normalizedWorks = isAuthorWorkEntries
-    ? (entries as AuthorWorkEntry[]).map((w) => ({
-        key: w.key.split("/").pop() || "",
-        title: w.title,
-        first_publish_year: w.first_publish_year,
-        covers: w.covers,
-        description: w.description,
-      }))
-    : (entries as Work[]).map((w) => ({
-        key: w.openlibrary_id,
-        title: w.title,
-        first_publish_year: w.first_publish_year,
-        covers: w.covers,
-        description: w.description,
-      }));
-
   return (
     <div className="flex flex-col">
       <div className="w-full bg-[#eae7da]">
@@ -63,42 +45,14 @@ export default async function Page({
           }
         />
       </div>
-
-      {entries.length > 0 ? (
-        <div className="px-24 flex py-8">
-          <div className="mx-16">
-            <h2 className="text-2xl font-semibold w-full mb-4">
-              Showing {author.name}'s works
-            </h2>
-            <div className="flex flex-col gap-4 max-w-4xl">
-              {normalizedWorks
-                .filter(
-                  (work) =>
-                    work.description && work.covers && work.covers.length > 0
-                )
-                .slice(0, 5)
-                .map((work) => (
-                  <WorkCard
-                    key={work.key}
-                    work_key={work.key}
-                    title={work.title}
-                    first_publish_year={work.first_publish_year}
-                    cover={
-                      work.covers && work.covers.length > 0
-                        ? getImage(work.covers[0].toString())
-                        : undefined
-                    }
-                    description={work.description || undefined}
-                  />
-                ))}
-            </div>
-          </div>
+      <div className="px-24 flex py-8">
+        <div className="mx-16">
+          <h2 className="text-2xl font-semibold w-full mb-4">
+            Showing {author.name}'s works
+          </h2>
+          <AuthorsWorksSection authorOlid={authorOlid} />
         </div>
-      ) : (
-        <div className="flex items-center justify-center h-96">
-          <Spinner className="size-8" />
-        </div>
-      )}
+      </div>
     </div>
   );
 }
---
+++ components/author/authors-works-section.tsx
@@ -0,0 +1,34 @@
+import { getAuthorWorks } from "@/api/author";
+import { getImage } from "@/api/work";
+import WorkCard from "@/components/search/WorkCard";
+
+export default async function AuthorsWorksSection({
+  authorOlid,
+}: {
+  authorOlid: string;
+}) {
+  const data = (await getAuthorWorks(authorOlid)).data;
+  const size = data.size;
+  const entries = data.entries;
+
+  return (
+    entries.length > 0 && (
+      <div className="flex flex-col gap-4 max-w-4xl">
+        {entries.slice(0, 10).map((work) => (
+          <WorkCard
+            key={work.key}
+            work_key={work.key}
+            title={work.title}
+            first_publish_year={work.first_publish_year}
+            cover={
+              work.covers && work.covers.length > 0
+                ? getImage(work.covers[0].toString())
+                : undefined
+            }
+            description={work.description || undefined}
+          />
+        ))}
+      </div>
+    )
+  );
+}
