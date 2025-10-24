f9b6c1904e126ae43b6484597457eeaa206dfe34
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-24 17:54:07 +0700

    fix(author): use correct type in api

--- api/author.ts
+++ api/author.ts
@@ -1,21 +1,21 @@
-import { GetAuthorResponse, GetAuthorWorkResponse } from "@/types/api";
-
 import { serverApiClient } from "@/lib/apiClient.server";
+import { GetAuthorsWorksResponse } from "@/types/api";
+import { GetAuthorResponse } from "@/types/api";
 
 export const getAuthor = async (
-  authorOlid: string
+  authorOlid: string,
 ): Promise<GetAuthorResponse> => {
   const response = await serverApiClient.get<GetAuthorResponse["data"]>(
-    `/authors/${authorOlid}`
+    `/authors/${authorOlid}`,
   );
   return response;
 };
 
 export const getAuthorWorks = async (
-  authorOlid: string
-): Promise<GetAuthorWorkResponse> => {
-  const response = await serverApiClient.get<GetAuthorWorkResponse["data"]>(
-    `/authors/${authorOlid}/works`
+  authorOlid: string,
+): Promise<GetAuthorsWorksResponse> => {
+  const response = await serverApiClient.get<GetAuthorsWorksResponse["data"]>(
+    `/authors/${authorOlid}/works`,
   );
   return response;
 };
