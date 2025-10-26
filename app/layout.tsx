b27a58d216a7551e2d5034e942fe13ed21c2a529
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-26 21:05:26 +0700

    chore: add speed analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { getMe } from "@/lib/auth";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "spellbound",
